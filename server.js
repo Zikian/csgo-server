1d2345681d5551bc93e3301668c3f92928f2bb2f
Zilian <ibroncha@Zilians-MacBook-Pro.local>
2022-06-02 00:51:19 +0200

    ok

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const fs = require('fs')
 
 app.set('port', 3000)
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+const ancientNades = JSON.parse(fs.readFileSync(__dirname + '/public/nades/ancient.json'));
+const mirageNades = JSON.parse(fs.readFileSync(__dirname + '/public/nades/mirage.json'));
+const overpassNades = JSON.parse(fs.readFileSync(__dirname + '/public/nades/overpass.json'));
 
 app.get('/ancient', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/ancient.html'))
+	res.render('radar.ejs', {nades : ancientNades, radarImg: "Ancient.png"})
+})
+
+app.get('/mirage', function(req, res) {
+	res.render('radar.ejs', {nades : mirageNades, radarImg: "Mirage.png"})
+})
+
+app.get('/overpass', function(req, res) {
+	res.render('radar.ejs', {nades : overpassNades, radarImg: "Overpass.png"})
 })
 
 app.listen(app.get('port'), function () {
